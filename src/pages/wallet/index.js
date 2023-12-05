e6f8b69214ecd03f3d935c2efc4fdb3f0be5cf68
Abhishek Kumar <abhishekmishraxyz@gmail.com>
2023-12-05 11:52:56 +0530

    .

--- src/pages/wallet/index.js
+++ src/pages/wallet/index.js
@@ -438,6 +438,7 @@ const Wallet = ({ depositAmount }) => {
             />
             <br />
             <br />
+            <p>{amount.toString()}</p>
             <button
               class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300"
               // onClick={handlePayment}
