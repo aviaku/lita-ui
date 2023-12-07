1a941971b55feb2046912f8afe26b163eff15e90
Abhishek Kumar <abhishekmishraxyz@gmail.com>
2023-12-07 12:38:02 +0530

    .

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -31,22 +31,23 @@ export default function LoginForm({ setVisible }) {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const loginSubmit = async () => {
-    try {
-      setLoading(true);
-      const { data } = await axios.post(
-        `${process.env.REACT_APP_BACKEND_URL}/login`,
-        {
-          email,
-          password,
-        }
-      );
-      dispatch({ type: "LOGIN", payload: data });
-      Cookies.set("user", JSON.stringify(data));
-      navigate("/");
-    } catch (error) {
-      setLoading(false);
-      setError(error?.response?.data?.message);
-    }
+    console.log("loginSubmit");
+    // try {
+    //   setLoading(true);
+    //   const { data } = await axios.post(
+    //     `${process.env.REACT_APP_BACKEND_URL}/login`,
+    //     {
+    //       email,
+    //       password,
+    //     }
+    //   );
+    //   dispatch({ type: "LOGIN", payload: data });
+    //   Cookies.set("user", JSON.stringify(data));
+    //   navigate("/");
+    // } catch (error) {
+    //   setLoading(false);
+    //   setError(error?.response?.data?.message);
+    // }
   };
   return (
     <div className="login_wrap">
